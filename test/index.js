b180417423f57da90c3036d82da2aebdcba9698f
Jackie Meng <mengjq08@gmail.com>
2017-05-10 22:39:46 +0800

    fix private api

--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-import assert from 'assert';
+// import assert from 'assert';
 import HamalBter from '../lib';
 
 const client = new HamalBter({
@@ -10,10 +10,18 @@ client.setupDefaultPair('cny', 'bts');
 
 describe('hamal-bter', function () {
   it('should have unit test!', function (done) {
-    assert(false, 'we expected this package author to add actual unit tests.');
-    client.orderBook().then(data => {
-      console.log(data);
-      done();
-    });
+    // assert(false, 'we expected this package author to add actual unit tests.');
+    // client.orderBook().then(data => {
+    //   console.log(data);
+    //   done();
+    // });
+    client
+      // .balances()
+      .buy(0.01, 1000)
+      // .buy(0.01, 1000, 'bts_cny')
+      .then(data => {
+        console.log(data);
+        done();
+      });
   });
 });
